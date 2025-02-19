d39b78ae1ed04dfe9dad36b1769097e281ddaf9d
Brice Wilson <Brice.Wilson@avodemia.com>
2025-02-19 17:06:25 +0300

    dynamically add the divs

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // console.log("hello world")
+let divContainer = document.querySelector(".container")
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        let grids = document.createElement("div")
+        // grids.textContent = "a"
+        grids.style.cssText = "width: 60px; height: 60px; text-align:center; border: 1px solid white"
+        grids.classList.add("gridSquare")
+        divContainer.appendChild(grids)
+    }
+}
